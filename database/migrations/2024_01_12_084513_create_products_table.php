d45b113bb4b3f8baf14ac7f2d8f76d2783576352
Hadi Rizaldi R <hadirizaldi.hr@gmail.com>
2024-01-12 18:35:30 +0700

    galleries and products table migration success

---
+++ database/migrations/2024_01_12_084513_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements("id");
+            $table->char('name', 100);
+            $table->char('slug', 100);
+            $table->char('type', 100);
+            $table->longText('description');
+            $table->integer('price');
+            $table->integer('quantity');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
