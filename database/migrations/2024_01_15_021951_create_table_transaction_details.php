7f3f804eaf2e1717192c75150a51c49ffaa408f8
Hadi Rizaldi R <hadirizaldi.hr@gmail.com>
2024-01-15 09:37:35 +0700

    install laravel ui vue for auth

---
+++ database/migrations/2024_01_15_021951_create_table_transaction_details.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('table_transaction_details', function (Blueprint $table) {
+            $table->bigInteger("id");
+            $table->integer("transaction_id");
+            $table->integer("product_id");
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('table_transaction_details');
+    }
+};
